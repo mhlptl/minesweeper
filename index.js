effd10a2f5f790ac89cca44dbab0e1d0bf62827f
Mehul Patel <mehulp1123@gmail.com>
2020-12-10 01:41:20 -0500

    css centering + replaced button with space

--- index.js
+++ index.js
@@ -5,27 +5,25 @@ for(let i = 0; i < 10; i++) {
     div.className = 'list-container';
     let list = document.createElement('ul');
     list.className = 'list'
-    for(let j = 0; j < 10; j++) {
+    for(let j = 0; j < 20; j++) {
         let listItem = document.createElement('li');
         listItem.className = 'list-item';
-        let button = document.createElement('button');
-        button.textContent = '*';
-        button.className = 'btn'
-        listItem.appendChild(button);
+        listItem.innerText = '\xA0';
         list.appendChild(listItem);
     }
     div.appendChild(list);
     board.appendChild(div);
 }
 
-let btn = document.getElementsByClassName('btn');
+let item = document.getElementsByClassName('list-item');
 
-for(let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('contextmenu', function(e) {
+for(let i = 0; i < item.length; i++) {
+    item[i].addEventListener('contextmenu', function(e) {
         e.preventDefault();
+        item[i].classList.toggle('flagged');
         console.log('right clicked!');
     })
-    btn[i].addEventListener('click', function(e) {
+    item[i].addEventListener('click', function(e) {
         console.log('left clicked!');
     })
 }
