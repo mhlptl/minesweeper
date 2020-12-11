923a827cf63e847906d507c001803e72f1dbf0ef
Mehul Patel <mehulp1123@gmail.com>
2020-12-10 22:06:37 -0500

    added win/loss functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let board = document.getElementById('inner-board');
 
+let interval;
+
 for(let i = 0; i < 10; i++) {
     let div = document.createElement('div');
     div.className = 'list-container';
@@ -12,7 +14,6 @@ for(let i = 0; i < 10; i++) {
         text.innerText = '\xA0';
         text.id = `text-${i}x${j}`;
         text.className = 'item-text';
-        // listItem.innerText = '\xA0';
         listItem.appendChild(text);
         listItem.id = `item-${i}x${j}`
         list.appendChild(listItem);
@@ -23,46 +24,108 @@ for(let i = 0; i < 10; i++) {
 
 let item = document.getElementsByClassName('list-item');
 
-for(let i = 0; i < item.length; i++) {
-    item[i].addEventListener('contextmenu', function(e) {
-        e.preventDefault();
-        item[i].classList.toggle('flagged');
-        changeFlagged();
-        console.log('right clicked!');
-    })
-    item[i].addEventListener('click', function(e) {
-        if(item[i].classList.contains('flagged')) {
-            return;
+let handleContextMenu = (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('flagged')) e.target.classList.remove('flagged');
+    else {
+        if(document.getElementsByClassName('flagged').length === numMines) return;
+        e.target.classList.toggle('flagged');
+        if(checkWin()) {
+            onWin();
+            clearListeners();
         }
-        let id = item[i].id;
-        let loc = id.substring(5);
-        let coords = loc.split('x');
-        let obj = {x: Number(coords[0]), y: Number(coords[1])};
-        if(checkDups(locations, obj)) {
-            item[i].classList.add('mine');
-        }
-        else {
-            if(checkSpot(locations, obj) === 0) {
-                let neighbors = []
-                getNeighbors(locations, obj, neighbors);
-                neighbors.forEach((val) => {
-                    document.getElementById(`item-${val}`).classList.add('safe');
-                });
-            }
-            else {
-                document.getElementById(`text-${loc}`).innerText = checkSpot(locations, obj);
-                item[i].classList.add('safe');
-            }
-        }
-        // console.log('left clicked!');
-    })
+    }
+    changeFlagged();
 }
 
+let handleClick = (e) => {
+    e.preventDefault();
+    target = e.target;
+    if(target.classList.contains('flagged')) return;
+    let obj = parseLocation(target.id);
+    if(checkDups(locations, obj)) {
+        target.classList.add('mine');
+        clearInterval(interval);
+        clearListeners();
+        let res = document.createElement('h1');
+        res.id = 'lost';
+        res.className = 'result';
+        res.textContent = 'Better Luck Next Time';
+        document.getElementById('container').appendChild(res);
+        document.getElementById('board').classList.add('opacity');
+    }
+    else {
+        if(checkSpot(locations, obj) === 0) {
+            let neighbors = [];
+            visited = [];
+            getNeighbors(locations, obj, neighbors);
+            neighbors.forEach((val) => {
+                document.getElementById(`item-${val}`).classList.add('safe');
+            });
+        }
+        else {
+            target.innerText = checkSpot(locations, obj);
+            target.classList.add('safe');
+        }
+        clearListener(e.currentTarget);
+    }
+    if(checkWin()) {
+        onWin();
+        clearListeners();
+    }
+}
+
+for(let i = 0; i < item.length; i++) {
+    item[i].addEventListener('contextmenu', handleContextMenu);
+    item[i].addEventListener('click', handleClick);
+}
+
+let onWin = () => {
+    let res = document.createElement('h1');
+    res.id = 'won';
+    res.className = 'result';
+    res.textContent = 'Great Job!';
+    document.getElementById('container').appendChild(res);
+    document.getElementById('board').classList.add('opacity');
+}
+
+let prettyPrint = (time) => {
+    let hours = "";
+    let minutes = "";
+    let seconds = "";
+    seconds = (time % 59).toString().padStart(2, '0');
+    time = Math.floor(time /= 59);
+    minutes = (time % 59).toString().padStart(2, '0');
+    time = Math.floor(time /= 23);
+    hours = (time % 59).toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+
+}
 
 let timeElapsed = () => {
-    
+    let count = 0;
+    interval = setInterval(() => {
+        document.getElementById('time-elapsed').innerText = `Time Elapsed: ${prettyPrint(count)}`;
+        count++;
+    }, 1000);
 }
 
 let changeFlagged = () => {
     document.getElementById('num-flagged').innerText =  (`Flags Remaining: ${10 - document.querySelectorAll('.flagged').length}`);
 }
+
+
+let clearListeners = () => {
+    for(let i = 0; i < item.length; i++) {
+        item[i].removeEventListener('click', handleClick);
+        item[i].removeEventListener('contextmenu', handleContextMenu);
+    }
+}
+
+let clearListener = (target) => {
+    target.removeEventListener('click', handleClick);
+    target.removeEventListener('contextmenu', handleContextMenu);
+}
+
+timeElapsed();
--- minesweeper.js
+++ minesweeper.js
@@ -46,7 +46,7 @@ let getNeighbors = (arr, obj, neighbors = []) => {
     let x = obj.x;
     let y = obj.y;
     let str = `${obj.x}x${obj.y}`;
-    if(x+1 >= 10 || y+1 >= 10 || x-1 < 0 || y-1 < 0 || visited.includes(str)) return;
+    if(x >= 10 || y >= 10 || x < 0 || y < 0 || visited.includes(str)) return;
 
     visited.push(str);
 
@@ -61,7 +61,22 @@ let getNeighbors = (arr, obj, neighbors = []) => {
         getNeighbors(arr, {x: x, y: y+1}, neighbors);
         getNeighbors(arr, {x: x, y: y-1}, neighbors);
     }
+}
 
+let checkWin = () => {
+    let flags = document.getElementsByClassName('flagged');
+    if(flags.length < numMines) return false;
+    for(let i = 0; i < flags.length; i++) {
+        let obj = parseLocation(flags[i].id);
+        if(!checkDups(locations, obj)) return false;
+    }
+    return true;
+}
+
+let parseLocation = (str) => {
+    let loc = str.substring(5);
+    let coords = loc.split('x');
+    return {x: Number(coords[0]), y: Number(coords[1])};
 }
 
 while(locations.length < numMines) {
@@ -70,5 +85,3 @@ while(locations.length < numMines) {
     let obj = {x: x, y: y};
     if(!checkDups(locations, obj)) locations.push(obj);
 }
-
-console.log(locations);
